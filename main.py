b609dc4e6e07f257af102190890a849fdde11305
Jose Pina <carlos_010396@hotmail.com>
2023-11-24 16:08:21 -0600

    Prueba de ejecución del entorno de Streamlit

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import streamlit as st
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    st.title('Esto es un título')
+    st.title('_Streamlit_ is :blue[cool] :sunglasses:')
 
-
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
-print_hi('José')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    print_hi('José')
