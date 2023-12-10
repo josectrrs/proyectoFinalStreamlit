96c8a0b8a918915b3d9dc6a646d1e2a598439527
Jose Pina <carlos_010396@hotmail.com>
2023-12-10 10:08:48 -0600

    Subida de archivos base e inicio de programación.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
-def print_hi(name):
-    st.title('Esto es un título')
-    st.title('_Streamlit_ is :blue[cool] :sunglasses:')
+#def print_hi(name):
+    #st.title('Esto es un título')
+    #st.title('_Streamlit_ is :blue[cool] :sunglasses:')
 
-if __name__ == '__main__':
-    print_hi('José')
+#if __name__ == '__main__':
+    #print_hi('José')
+
+st.title('Proyecto Final - Validador de RFC')
+st.subheader("Integrantes: José Carlos Piña Torres y Wilbert Ernesto Ake Berzunza")
+
+
+
