1e503e391ec69cdea8f83fee77205f6300d5442e
Jose Pina <carlos_010396@hotmail.com>
2023-12-10 23:53:37 -0600

    Nueva edición del expander Paso 2.

--- FAQPage.py
+++ FAQPage.py
@@ -56,7 +56,7 @@ elif selected2 == "Paso 2":
             
         """)
 
-        st.image("https://drive.google.com/file/d/1_y9d2_ckx2c6zw15TOe1s3iPgtyRA9Mk/view?usp=sharing", caption="Carga de Datos", use_column_width=True)
+        st.image("imgs/1_PF.png", caption="Carga de Datos", use_column_width=True)
 
 # Expander para Paso 3
 elif selected2 == "Paso 3":
