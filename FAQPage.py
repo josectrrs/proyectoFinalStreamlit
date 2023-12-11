e535eb420866763f92f9d9fbf620d926393d80db
Jose Pina <carlos_010396@hotmail.com>
2023-12-10 21:54:50 -0600

    Nueva modificación para utilizar streamlit-option-menu

--- FAQPage.py
+++ FAQPage.py
@@ -4,7 +4,7 @@ from streamlit_option_menu import option_menu
 st.title("Preguntas Frecuentes (FAQ) - Proyecto Final")
 
 # 2. horizontal menu
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
+selected2 = option_menu(None, ["Paso 1", "Paso 2", "Paso 3", 'Paso 4'],
                         icons=['house', 'cloud-upload', "list-task", 'gear'],
                         menu_icon="cast", default_index=0, orientation="horizontal")
 selected2
