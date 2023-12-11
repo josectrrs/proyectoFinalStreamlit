69d24c00a753940fc162efd27aec597029305acd
Jose Pina <carlos_010396@hotmail.com>
2023-12-10 23:14:26 -0600

    Nueva customización de todos los expander.

--- FAQPage.py
+++ FAQPage.py
@@ -24,22 +24,37 @@ if selected2 == "Paso 1":
 elif selected2 == "Paso 2":
     with st.expander("Ver explicación del Paso 2"):
         st.write("""
-            Aquí va la explicación detallada del Paso 2.
-            Puedes agregar texto, imágenes o cualquier otro contenido.
+            :pencil: **Limpieza de los datos** 
+            
+            :green[**¿Qué datos están trabajando?:**]
+            Estamos trabajando con datos de clientes del sistema de "Factura Electrónica de CONTPAQi". Estos datos incluyen información relacionada con los clientes que utilizan el sistema, como sus nombres, Registro Federal de Contribuyentes (RFC), razón social, Régimen Fiscal, Código Postal y otros detalles relevantes. Estos datos son esenciales para la emisión de facturas electrónicas y deben ser validados ante el Servicio de Administración Tributaria (SAT) para asegurar su corrección y cumplimiento con las regulaciones fiscales.
+
+            :green[**¿De dónde se obtuvieron?:**]
+            Los datos fueron obtenidos a través de una base de datos de clientes del sistema de Factura Electrónica de CONTPAQi. Esta base de datos contiene información sobre diversas empresas que utilizan el sistema y que han registrado sus datos para la emisión de facturas electrónicas.
         """)
 # Expander para Paso 3
 elif selected2 == "Paso 3":
     with st.expander("Ver explicación del Paso 3"):
         st.write("""
-            Aquí va la explicación detallada del Paso 3.
-            Puedes agregar texto, imágenes o cualquier otro contenido.
+            :pencil: **Análisis de los datos** 
+            
+            :green[**¿Qué datos están trabajando?:**]
+            Estamos trabajando con datos de clientes del sistema de "Factura Electrónica de CONTPAQi". Estos datos incluyen información relacionada con los clientes que utilizan el sistema, como sus nombres, Registro Federal de Contribuyentes (RFC), razón social, Régimen Fiscal, Código Postal y otros detalles relevantes. Estos datos son esenciales para la emisión de facturas electrónicas y deben ser validados ante el Servicio de Administración Tributaria (SAT) para asegurar su corrección y cumplimiento con las regulaciones fiscales.
+
+            :green[**¿De dónde se obtuvieron?:**]
+            Los datos fueron obtenidos a través de una base de datos de clientes del sistema de Factura Electrónica de CONTPAQi. Esta base de datos contiene información sobre diversas empresas que utilizan el sistema y que han registrado sus datos para la emisión de facturas electrónicas.
         """)
 # Expander para Paso 4
 elif selected2 == "Paso 4":
     with st.expander("Ver explicación del Paso 4"):
         st.write("""
-            Aquí va la explicación detallada del Paso 4.
-            Puedes agregar texto, imágenes o cualquier otro contenido.
+            :pencil: **Visualización de los datos** 
+            
+            :green[**¿Qué datos están trabajando?:**]
+            Estamos trabajando con datos de clientes del sistema de "Factura Electrónica de CONTPAQi". Estos datos incluyen información relacionada con los clientes que utilizan el sistema, como sus nombres, Registro Federal de Contribuyentes (RFC), razón social, Régimen Fiscal, Código Postal y otros detalles relevantes. Estos datos son esenciales para la emisión de facturas electrónicas y deben ser validados ante el Servicio de Administración Tributaria (SAT) para asegurar su corrección y cumplimiento con las regulaciones fiscales.
+
+            :green[**¿De dónde se obtuvieron?:**]
+            Los datos fueron obtenidos a través de una base de datos de clientes del sistema de Factura Electrónica de CONTPAQi. Esta base de datos contiene información sobre diversas empresas que utilizan el sistema y que han registrado sus datos para la emisión de facturas electrónicas.
         """)
 
 selected2
