7f1fab06940c4ec351e967b2161efadbac9f5eab
Jose Pina <carlos_010396@hotmail.com>
2023-12-10 22:10:56 -0600

    Nueva modificación en el archivo FAQPage.py

--- FAQPage.py
+++ FAQPage.py
@@ -5,7 +5,6 @@ st.title("Preguntas Frecuentes (FAQ) - Proyecto Final")
 
 # 2. horizontal menu
 selected2 = option_menu(None, ["Paso 1", "Paso 2", "Paso 3", 'Paso 4'],
-                        icons=['house', 'cloud-upload', "list-task", 'gear'],
                         menu_icon="cast", default_index=0, orientation="horizontal")
 
 # Expander para Paso 1
