1e14ac28ab1ef5601c2c441f82e6253195bf1f37
Jose Pina <carlos_010396@hotmail.com>
2023-12-11 19:25:53 -0600

    Re-formulación del código del archivo auxiliarPage.py

--- auxiliarPage.py
+++ auxiliarPage.py
@@ -2,52 +2,45 @@
 import streamlit as st
 import pandas as pd
 import os
-
+import matplotlib.pyplot as plt
 
 # Función para buscar el RFC en el archivo de texto y mostrar el resultado con colores
-def buscar_rfc_en_archivo_txt(excel_df, selected_reason_social, txt_content):
-    rfc_column = excel_df.loc[excel_df["Razón Social"] == selected_reason_social, "R.F.C."]
-    if not rfc_column.empty:
-        rfc_to_search = rfc_column.iloc[0]
+def buscar_rfc_en_archivo_txt(excel_df, selected_reason_social, txt_content, manual_rfc=None):
+    rfc_to_search = manual_rfc or excel_df.loc[excel_df["Razón Social"] == selected_reason_social, "R.F.C."].iloc[0]
 
-        # Buscar el RFC correspondiente en el archivo de texto
-        lines = txt_content.split('\n')
-        for line in lines:
-            parts = line.split('|')
-            if len(parts) == 3 and parts[1] == rfc_to_search:
-                status_text = parts[2].strip()
-                if "RFC válido" in status_text:
-                    #st.success(f"RFC {rfc_to_search} es válido ante el SAT. Detalles:\n{line}")
+    # Buscar el RFC correspondiente en el archivo de texto
+    lines = txt_content.split('\n')
+    for line in lines:
+        parts = line.split('|')
+        if len(parts) == 3 and parts[1] == rfc_to_search:
+            status_text = parts[2].strip()
+            if "RFC válido" in status_text:
+                # Obtener Razón Social del RFC desde el archivo Excel
+                razon_social = excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Razón Social"].iloc[0]
 
-                    # Obtener Régimen Fiscal y Código Postal del RFC desde el archivo Excel
-                    regimen_fiscal = excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Régimen Fiscal"].iloc[0]
-                    codigo_postal = int(excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Código Postal"].iloc[0])
+                # Mostrar los datos en una tabla
+                st.write("Detalles del RFC:")
+                df_details = pd.DataFrame({"R.F.C.": [rfc_to_search], "Razón Social": [razon_social],
+                                           "Régimen Fiscal": [excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Régimen Fiscal"].iloc[0]],
+                                           "Código Postal": [int(excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Código Postal"].iloc[0])],
+                                           "Respuesta del SAT": [status_text]})
+                st.dataframe(df_details.style.applymap(color_cell, subset=["Respuesta del SAT"]))
+            elif "no registrado en el padrón de contribuyentes" in status_text:
+                # Obtener Razón Social del RFC desde el archivo Excel
+                razon_social = excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Razón Social"].iloc[0]
 
-                    # Mostrar los datos en una tabla
-                    st.write("Detalles del RFC:")
-                    df_details = pd.DataFrame({"R.F.C.": [rfc_to_search], "Régimen Fiscal": [regimen_fiscal],
-                                               "Código Postal": [codigo_postal], "Respuesta del SAT": [status_text]})
-                    st.dataframe(df_details.style.applymap(color_cell, subset=["Respuesta del SAT"]))
-                elif "no registrado en el padrón de contribuyentes" in status_text:
-                    #st.error(f"RFC {rfc_to_search} no registrado en el padrón de contribuyentes. Detalles:\n{line}")
-
-                    # Obtener Régimen Fiscal y Código Postal del RFC desde el archivo Excel
-                    regimen_fiscal = excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Régimen Fiscal"].iloc[0]
-                    codigo_postal = int(excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Código Postal"].iloc[0])
-
-                    # Mostrar los datos en una tabla con color rojo
-                    st.write("Detalles del RFC (No Válido):")
-                    df_details = pd.DataFrame({"R.F.C.": [rfc_to_search], "Régimen Fiscal": [regimen_fiscal],
-                                               "Código Postal": [codigo_postal], "Respuesta del SAT": [status_text]})
-                    st.dataframe(df_details.style.applymap(color_cell_red, subset=["Respuesta del SAT"]))
-                else:
-                    st.warning(f"RFC {rfc_to_search} tiene un estado desconocido. Detalles:\n{line}")
-                break
-        else:
-            st.warning(f"RFC {rfc_to_search} no encontrado en el archivo de texto.")
+                # Mostrar los datos en una tabla con color rojo
+                st.write("Detalles del RFC (No Válido):")
+                df_details = pd.DataFrame({"R.F.C.": [rfc_to_search], "Razón Social": [razon_social],
+                                           "Régimen Fiscal": [excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Régimen Fiscal"].iloc[0]],
+                                           "Código Postal": [int(excel_df.loc[excel_df["R.F.C."] == rfc_to_search, "Código Postal"].iloc[0])],
+                                           "Respuesta del SAT": [status_text]})
+                st.dataframe(df_details.style.applymap(color_cell_red, subset=["Respuesta del SAT"]))
+            else:
+                st.warning(f"RFC {rfc_to_search} tiene un estado desconocido. Detalles:\n{line}")
+            break
     else:
-        st.error("Error al obtener el RFC desde el archivo Excel.")
-
+        st.warning(f"RFC {rfc_to_search} no encontrado en el archivo de texto.")
 
 # Función para aplicar colores a las celdas en función de la respuesta del SAT (color verde)
 def color_cell(value):
@@ -84,18 +77,37 @@ def auxiliarPage(username):
 
         excel_df = pd.read_excel(os.path.join("archivos", selected_excel_file))
 
-        # Seleccionar razón social desde el archivo Excel
-        selected_reason_social = st.selectbox("Seleccionar Razón Social", excel_df["Razón Social"].tolist(),
-                                              key="selectbox")
+        # Sección para ingresar manualmente un RFC
+        st.subheader("Búsqueda manual de RFC")
+        manual_rfc = st.text_input("Ingresar RFC manualmente")
 
-        # Botón para realizar la búsqueda
-        if st.button("Comprobar estado de RFC"):
-            # Buscar el RFC correspondiente en el archivo de texto
-            txt_file_path = os.path.join("archivos", "RESPUESTA_SAT_RFC.txt")
-            with open(txt_file_path, "r", encoding='latin-1') as txt_file:
-                txt_content = txt_file.read()
+        if st.button("Buscar RFC"):
+            # Verificar si se ingresó un RFC
+            if not manual_rfc:
+                st.warning("Por favor, ingresa un RFC antes de buscar.")
+            else:
+                # Buscar el RFC correspondiente en el archivo de texto
+                txt_file_path = os.path.join("archivos", "RESPUESTA_SAT_RFC.txt")
+                with open(txt_file_path, "r", encoding='latin-1') as txt_file:
+                    txt_content = txt_file.read()
 
-            buscar_rfc_en_archivo_txt(excel_df, selected_reason_social, txt_content)
+                # Mostrar resultados de la búsqueda manual
+                buscar_rfc_en_archivo_txt(excel_df, "", txt_content, manual_rfc)
+
+                # Calcula las proporciones de RFC válidos y no válidos
+                validos = txt_content.count("RFC válido")
+                no_validos = txt_content.count("no registrado en el padrón de contribuyentes")
+
+                # Crea el gráfico de pastel con números en lugar de porcentajes
+                labels = [f'RFC Válidos ({validos})', f'RFC No Válidos ({no_validos})']
+                sizes = [validos, no_validos]
+                colors = ['#8eff8e', '#ff8e8e']
+
+                fig, ax = plt.subplots()
+                ax.pie(sizes, labels=labels, startangle=90, colors=colors)
+                ax.axis('equal')
+
+                st.pyplot(fig)
 
     # Botón para cerrar sesión
     if st.button("Cerrar Sesión"):
